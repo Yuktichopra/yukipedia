595a541cc0560e9fa79d38ab586da1cfd172aeb3
Yukti Chopra <choprayukti9498@gmail.com>
2024-09-30 16:19:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Hover effects for blogs
+const blogArticles = document.querySelectorAll('.blog');
+blogArticles.forEach(blog => {
+    blog.addEventListener('mouseover', () => {
+        blog.style.boxShadow = '0px 5px 15px rgba(0, 128, 0, 0.5)';
+    });
+    blog.addEventListener('mouseout', () => {
+        blog.style.boxShadow = 'none';
+    });
+});
