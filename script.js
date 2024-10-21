9b2f06bbc4317fd73d5c9f6a09498860889f94cb
Yukti Chopra <choprayukti9498@gmail.com>
2024-10-21 04:32:11 +0200

    some changes about the layout

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Smooth scrolling for navigation
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
@@ -18,3 +19,25 @@ blogArticles.forEach(blog => {
         blog.style.boxShadow = 'none';
     });
 });
+=======
+// Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Hover effects for blogs
+const blogArticles = document.querySelectorAll('.blog');
+blogArticles.forEach(blog => {
+    blog.addEventListener('mouseover', () => {
+        blog.style.boxShadow = '0px 5px 15px rgba(0, 128, 0, 0.5)';
+    });
+    blog.addEventListener('mouseout', () => {
+        blog.style.boxShadow = 'none';
+    });
+});
+>>>>>>> d2bf917 (some changes about the layout)
